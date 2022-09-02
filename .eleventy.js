8497072f72013a80b7139c291cfc237d9f0badc3
txtsd <thexerothermicsclerodermoid@gmail.com>
2022-09-02 12:30:46 +0530

    feat: Setup 11ty for the blog
    Signed-off-by: txtsd <thexerothermicsclerodermoid@gmail.com>

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+const moment = require('moment');
+moment.locale('en');
+
+module.exports = function(eleventyConfig) {
+  // Date formatting
+  // from: https://keepinguptodate.com/pages/2019/06/creating-blog-with-eleventy/
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+
+  // Date formatting
+  // from: https://keepinguptodate.com/pages/2019/06/creating-blog-with-eleventy/
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).utc().format('LL'); // E.g. May 31, 2019
+  });
+
+  // CSS Passthrough
+  eleventyConfig.addPassthroughCopy('css')
+
+  return {
+    passthroughFileCopy: true
+  }
+};
